07e99a4bc32ecea0e4f27ca0a78de216023bc72f
Peter <thecookedhan@gmail.com>
2026-02-26 10:10:26 +0100

    some quick fixes

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ def index():
 
 @app.route('/solve', methods=['POST'])
 def solve():
-  data = request.json
-  start_time = time.time()
+  params = request.json
+  start_time = time.perf_counter()
 
   # tu wywołamy algorytm genetyczny
 
-  end_time = time.time()
-  execution_time = round(end_time - start_time, 4)
+  end_time = time.perf_counter()
+  execution_time = end_time - start_time
 
   # przykładowe wyniki do zwrócenia przez funkcję
   mock_results = {
-    "status" : "success",
-    "time" : execution_time,
+    "time" : f"{execution_time:.4f} s",
     "best_fitness" : 0.001,
     "chart_data" : [10, 8, 3, 1, 0.5, 0.001]
   }
