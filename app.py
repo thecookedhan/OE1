b56f046dbecc7a958f882449481207e1ac32dd03
Peter <thecookedhan@gmail.com>
2026-02-26 10:16:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('/templates/index.html')
+  return render_template('templates/index.html')
 
 @app.route('/solve', methods=['POST'])
 def solve():
