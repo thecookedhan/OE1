429d1b5dc6ef1d8c3fc54c0761688b056379dd3a
Zuzanna Brauer <entropia.b137@gmail.com>
2026-03-07 22:02:56 +0100

    finished genetic algorithm operators file + overall syntax improvements

--- genetic_algorithm/classes.py
+++ genetic_algorithm/classes.py
@@ -1,14 +1,14 @@
 import random
 
 class Individual:
-    def __init__(self, genome: list[int], fitness = None):
+    def __init__(self, genome: list[int], fitness: float = None):
         if not genome:
             raise ValueError("Genome cannot be empty")
 
         self.genome = genome
         self.fitness = fitness
 
-    def decode(self, bounds, bits_per_variable):
+    def decode(self, bounds: list[float], bits_per_variable: int) -> float:
         decimal_genome = []
 
         genome_splitted_by_variables = [self.genome[i:i + bits_per_variable] for i in range(0, len(self.genome), bits_per_variable)]
@@ -19,17 +19,17 @@ class Individual:
 
         return decimal_genome
 
-    def evaluate(self, fitness_function, bounds, bits_per_variable):
+    def evaluate(self, fitness_function, bounds: list[float], bits_per_variable: int):
         decoded_genome = self.decode(bounds, bits_per_variable)
         self.fitness = fitness_function(*decoded_genome)
 
-    def copy(self):
+    def copy(self) -> Individual:
         return Individual(self.genome.copy(), self.fitness)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.genome)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Individual(genome = {self.genome}, fitness = {self.fitness})'
     
 class Population:
@@ -43,21 +43,21 @@ class Population:
             raise ValueError("Population size must be positive")
         self.population_size = population_size
 
-    def initialize(self, genome_length):
+    def initialize(self, genome_length: int):
         self.clear()
 
         for _ in range(self.population_size):
             genome = [random.randint(0, 1) for _ in range(genome_length)]
             self.add_individual(Individual(genome))
 
-    def evaluate(self, fitness_function, bounds, bits_per_variable):
+    def evaluate(self, fitness_function, bounds: list[float], bits_per_variable: int):
         for individual in self.individuals:
             individual.evaluate(fitness_function, bounds, bits_per_variable)
 
-    def get_best_individual(self):
+    def get_best_individual(self) -> Individual:
         return max(self.individuals, key=lambda ind: ind.fitness)
     
-    def get_worst_individual(self):
+    def get_worst_individual(self) -> Individual:
         return min(self.individuals, key=lambda ind: ind.fitness)
 
     def add_individual(self, individual: Individual):
@@ -69,11 +69,11 @@ class Population:
     def clear(self):
         self.individuals = []
 
-    def sort(self, descending):
+    def sort(self, descending: bool):
         self.individuals.sort(key = lambda ind: ind.fitness, reverse = descending)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.individuals)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Population(individuals = {self.individuals}, size = {self.population_size})'
--- genetic_algorithm/ga_operators.py
+++ genetic_algorithm/ga_operators.py
@@ -1,8 +1,89 @@
-def mutation():
-    pass
+from classes import Individual
+from classes import Population
+import random
 
-def selection():
-    pass
+# === MUTATION ===
+def bit_flip_mutation(individual: Individual, mutation_rate: float = 0.01):
+    for locus in range(len(individual)):
+        if random.random() < mutation_rate:
+            individual.genome[locus] = 1 - individual.genome[locus]
+    
+    individual.fitness = None
 
-def crossover():
-    pass
+def two_bits_flip_mutation(individual: Individual):
+    random_indexes = random.sample(range(len(individual)), 2)
+
+    individual.genome[random_indexes[0]] = 1 - individual.genome[random_indexes[0]]
+    individual.genome[random_indexes[1]] = 1 - individual.genome[random_indexes[1]]
+
+    individual.fitness = None
+
+def edge_mutation(individual: Individual):
+    individual.genome[0] = 1 - individual.genome[0]
+    individual.genome[-1] = 1 - individual.genome[-1]
+
+    individual.fitness = None
+
+# === SELECTION ===
+def tournament_selection(population: Population, tournament_size: int = 3) -> Individual:
+    tournament_group = random.sample(population.individuals, tournament_size)
+    best_individual = max(tournament_group, key=lambda individual: individual.fitness)
+
+    return best_individual.copy()
+
+def best_selection(population: Population, best_percentage: float = 0.3):
+    population.sort(descending = True)
+    best_individuals_start_index = int(len(population) * best_percentage)
+    return [individual.copy() for individual in population.individuals[:best_individuals_start_index]]
+
+def roulette_selection(population: Population) -> Individual:
+    population_fitness_sum = sum([1/individual.fitness for individual in population])
+    random_fitness = random.uniform(0, population_fitness_sum)
+
+    cumulative_fintess_sum = 0
+    for individual in population.individuals:
+        cumulative_fintess_sum += individual.fitness
+        if cumulative_fintess_sum >= random_fitness:
+            return individual.copy()
+
+# === CROSSOVER ===
+def one_point_crossover(parent1: Individual, parent2: Individual):
+    crossover_point = random.randint(0, len(parent1) - 1)
+
+    child1_genome = parent1.genome[:crossover_point] + parent2.genome[crossover_point:]
+    child2_genome = parent2.genome[:crossover_point] + parent1.genome[crossover_point:]
+
+    return Individual(child1_genome), Individual(child2_genome)
+
+def two_point_crossover(parent1: Individual, parent2: Individual):
+    crossover_points = sorted(random.sample(range(len(parent1)), 2))
+
+    child1_genome = parent1.genome[:crossover_points[0]] + parent2.genome[crossover_points[0]:crossover_points[1]] + parent1.genome[crossover_points[1]:]
+    child2_genome = parent2.genome[:crossover_points[0]] + parent1.genome[crossover_points[0]:crossover_points[1]] + parent2.genome[crossover_points[1]:]
+
+    return Individual(child1_genome), Individual(child2_genome)
+
+def discrete_crossover(parent1: Individual, parent2: Individual) -> Individual:
+    child_genome = []
+
+    for locus in range(0, len(parent1)):
+        if random.random() <= 0.5:
+            child_genome.append(parent1.genome[locus])
+        else:
+            child_genome.append(parent2.genome[locus])
+    
+    return Individual(child_genome)
+
+def uniform_crossover(parent1: Individual, parent2: Individual, crossover_rate: float = 0.1):
+    child1_genome = []
+    child2_genome = []
+
+    for locus in range(0, len(parent1)):
+        if random.random() < crossover_rate:
+            child1_genome.append(parent1.genome[locus])
+            child2_genome.append(parent2.genome[locus])
+        else:
+            child1_genome.append(parent2.genome[locus])
+            child2_genome.append(parent1.genome[locus])
+    
+    return Individual(child1_genome), Individual(child2_genome)
